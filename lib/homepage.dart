31b4303f73328cd5806f8732a7922bfb98962eb4
SKGHD <sangramgorai@gmail.com>
2021-05-03 01:48:37 +0530

    Initialized repo

---
+++ lib/homepage.dart
@@ -0,0 +1,36 @@
+import 'package:covid19tracker/datasource.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('COVID-19 TRACKER'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              color: Colors.orange[100],
+              child: Text(DataSource.quote,
+                  style: TextStyle(
+                      color: Colors.orange[800],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
