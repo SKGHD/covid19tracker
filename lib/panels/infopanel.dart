038255c4956e04507f4ecda4e3b83d3989c27978
SKGHD <sangramgorai@gmail.com>
2021-08-30 22:17:48 +0530

    started infopanels

---
+++ lib/panels/infopanel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InfoPanel extends StatefulWidget {
+  InfoPanel({Key key}) : super(key: key);
+
+  @override
+  _InfoPanelState createState() => _InfoPanelState();
+}
+
+class _InfoPanelState extends State<InfoPanel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
